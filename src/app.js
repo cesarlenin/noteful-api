4a4c95cf1a10bc8e20e134fc920de9a36da87421
Cesar Morales <cesarlenin2@live.com>
2020-05-11 13:41:45 -0700

    set dbs

--- src/app.js
+++ src/app.js
@@ -3,31 +3,37 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+const { NODE_ENV} = require('./config');
+const validateBearerToken = require('./validate-bearer-token');
+const errorHandler = require('./error-handler');
+const bookmarksRouter = require('./bookmarks-router');
+
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
+// Middlewares
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(validateBearerToken);
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+app.use('/api/bookmarks',bookmarksRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
-
+app.use(errorHandler);
 module.exports = app;
